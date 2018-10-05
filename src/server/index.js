a5cc25201d5a25a60d4aff6bc31a63e045f859f5
Lucian M <munteanu.lucian.andrei@gmail.com>
2018-10-05 18:24:21 +0300

    Minor structure change

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@
 // node-seed
 import express from 'express';
 import type {$Response, $Request} from 'express';
-import logger from './middleware/logger'
+import logger from './interceptors/logger'
 
 const app = express();
 const PORT = 3000;
@@ -13,6 +13,6 @@ app.use(logger);
 // Test api 
 app.get('/', (req: $Request , resp: $Response) => {
     resp.send("Node-seed")
-})
+});
 
 app.listen(PORT, ()=> console.log("Server started on port ",PORT));
